94cbcec8266c8b8b00e9c5307dd31c75354033ad
doch <doch.carpet@gmail.com>
2026-08-28 12:51:45 +0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,41 @@
+export function formatPrice(value) {
 
+    return Number(value || 0).toLocaleString(
+        "en-US",
+        {
+            maximumFractionDigits: 0
+        }
+    );
+
+}
+
+
+export function formatNumber(value) {
+
+    const number = Number(value);
+
+    if (Number.isInteger(number)) {
+        return number;
+    }
+
+    return number
+        .toFixed(1)
+        .replace(".0", "");
+
+}
+
+
+export function escapeHtml(value) {
+
+    return String(value ?? "").replace(
+        /[&<>"']/g,
+        character => ({
+            "&": "&amp;",
+            "<": "&lt;",
+            ">": "&gt;",
+            '"': "&quot;",
+            "'": "&#039;"
+        }[character])
+    );
+
+}
